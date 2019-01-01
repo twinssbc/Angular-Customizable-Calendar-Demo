7f01a617a15795a8a6f3bdb81a4ed176341ad6a6
twinssbc <bocong@hotmail.com>
2019-01-01 18:17:55 +0800

    add sample code to use angular-customizable-calendar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { NgCalendarModule  } from 'angular-customizable-calendar';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgCalendarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
